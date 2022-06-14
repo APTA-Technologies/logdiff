b1bfd3863107a81a4c27d945cb9db5a00c3d24fc
Tom Catshoek <tomcatshoek@zeelandnet.nl>
2022-06-14 16:28:48 +0200

    Basic backend comms

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
 FROM ghcr.io/tudelft-cda-lab/flexfringe:main
 
+USER root
+RUN set -ex && \
+    apk add --no-cache jq
+
 # TODO
-# copy steps.sh
-# WORKDIR /home/flexfringe
-ENTRYPOINT ["/home/steps.sh"]
+
+USER flexfringe
+WORKDIR /home/flexfringe
+COPY steps.sh .
+ENTRYPOINT ["/home/flexfringe/steps.sh"]
--- steps.sh
+++ steps.sh
@@ -1,10 +1,35 @@
-#!/bin/bash
+#!/bin/sh
+
+PREVIOUS_MODEL_PATH="/home/flexfringe/previous.json"
 
 # (1) make test -> logfiles (test repo)
-# (2)  download previous model (action)
-# (3) run flexfringe logdiff on logs and downloaded model (action)
-# (4) learn new model from logs (action)
-# (5) push new model to database (action)
-# (6) publish logdiff results (action) 
+# Happens outside this action
 
+# (2)  download previous model (action)
+wget https://logdiff.herokuapp.com/api/latest -O $PREVIOUS_MODEL_PATH
+
+# (3) run flexfringe logdiff on logs and downloaded model (action)
+echo "Previous model:"
+cat $PREVIOUS_MODEL_PATH | jq
+echo "RUNNING LOGDIFF (TODO)"
+
+echo "---------------------------------------------------------"
+
+# (4) learn new model from logs (action)
+/home/flexfringe/flexfringe "$@"
+
+echo "---------------------------------------------------------"
+
+# (5) push new model to database (action)
+OUTPUT_PATH="$1.ff.final.json"
+echo "Output path: $OUTPUT_PATH"
+wget \
+    --header 'Content-Type: application/json' \
+    --post-file "$OUTPUT_PATH" \
+    https://logdiff.herokuapp.com/api/latest
+
+echo "---------------------------------------------------------"
+
+# (6) publish logdiff results (action) 
+echo "Publishing logdiff results... (TODO)"
 
