7379e5d0d47125143b83df9f048d8d61e2d22c6a
Chris Hammerschmidt <chris@apta.tech>
2022-06-14 15:59:52 +0200

    entrypiont

---
+++ steps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# (1) make test -> logfiles (test repo)
+# (2)  download previous model (action)
+# (3) run flexfringe logdiff on logs and downloaded model (action)
+# (4) learn new model from logs (action)
+# (5) push new model to database (action)
+# (6) publish logdiff results (action) 
+
+
